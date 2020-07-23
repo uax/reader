c46fbf014a5a00b0c39b43837cc155c7c12a8f2d
Noah <noah@zixi.net>
2020-07-24 02:31:38 +0800

    增加 Swiper & Icon

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,73 @@
 const app = getApp()
 
 Page({
-  data: {},
+  data: {
+    cardCur: 0,
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84000.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big84001.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big39000.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big10001.jpg'
+    }, {
+      id: 4,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big25011.jpg'
+    }, {
+      id: 5,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big21016.jpg'
+    }, {
+      id: 6,
+      type: 'image',
+      url: 'https://ossweb-img.qq.com/images/lol/web201310/skin/big99008.jpg'
+    }],
+    gridCol: 5,
+    iconList: [{
+      icon: 'cardboardfill',
+      color: 'red',
+      badge: 120,
+      name: '会员'
+    }, {
+      icon: 'recordfill',
+      color: 'yellow',
+      badge: 1,
+      name: '排行'
+    }, {
+      icon: 'picfill',
+      color: 'mauve',
+      badge: 0,
+      name: '完本'
+    }, {
+      icon: 'noticefill',
+      color: 'olive',
+      badge: 22,
+      name: '故事'
+    }, {
+      icon: 'upstagefill',
+      color: 'orange',
+      badge: 0,
+      name: '免费'
+    }],
+  },
 
   onLoad: function () {
     console.log('onLoad')
   },
-
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
 })
